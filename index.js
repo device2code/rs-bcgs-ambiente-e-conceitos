20ed44782fc2e7b3ee59084856a873cdc17ad4f9
André C. Scherrer <device2code@gmail.com>
2020-03-28 21:14:26 -0300

    query & route params

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const server = express();
 
-server.get('/users', (req, res) => {
-  return res.json({message: 'Hello World'});
+server.get('/users/:id', (req, res) => {
+  //const nome = req.query.nome; //query params ?nome=Andre
+  //const id = req.params.id; //route params /users/1
+  const { id } = req.params; 
+
+  return res.json({message: `Buscando o usuário ${id}`});
 });
 
 server.listen(3000);
