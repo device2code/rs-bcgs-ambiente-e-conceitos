e10c1b2a8b3b1be044ea1d96ff572a8ebf74a738
André C. Scherrer <device2code@gmail.com>
2020-03-28 23:47:18 -0300

    crud

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 const users = ['Roger', 'André', 'Eduardo', 'Ricardo', 'Priscila'];
+
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
 
 server.get('/users/:index', (req, res) => {
   //const nome = req.query.nome; //query params ?nome=Andre
@@ -11,6 +17,31 @@ server.get('/users/:index', (req, res) => {
   const { index } = req.params;
 
   return res.json(users[index]);
+});
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+  
+  users.splice(index, 1);
+
+  return res.send();
 });
 
 server.listen(3000);
