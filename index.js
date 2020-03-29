819b72e0d4da2abf29a857830e00dcfcd13712fe
André C. Scherrer <device2code@gmail.com>
2020-03-28 21:30:05 -0300

    utilizando insomnia

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const server = express();
 
-server.get('/users/:id', (req, res) => {
+const users = ['Roger', 'André', 'Eduardo', 'Ricardo'];
+
+server.get('/users/:index', (req, res) => {
   //const nome = req.query.nome; //query params ?nome=Andre
   //const id = req.params.id; //route params /users/1
-  const { id } = req.params; 
+  //const { id } = req.params; 
+  const { index } = req.params;
 
-  return res.json({message: `Buscando o usuário ${id}`});
+  return res.json(users[index]);
 });
 
 server.listen(3000);
